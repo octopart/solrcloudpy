a02e6068120dff48448e2dc97eb12c3aa7d3a47a
Abdul Hassan <abdulrhass@gmail.com>
2014-11-10 11:18:17 -0500

    bumped to version 1.4.463octo

--- solrcloudpy/__init__.py
+++ solrcloudpy/__init__.py
@@ -2,5 +2,5 @@ from solrcloudpy.connection import SolrConnection
 from solrcloudpy.collection import SolrCollection
 from solrcloudpy.parameters import SearchOptions
 
-__version__ = "1.4.462octo"
+__version__ = "1.4.463octo"
 __all__ = ['SolrCollection','SolrConnection','SearchOptions']
